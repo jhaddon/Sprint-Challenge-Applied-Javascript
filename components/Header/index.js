a03ad64ce782bd28653d1cfa6cae8a7c03d5f2d6
Joseph Haddon <jdhaddon@gmail.com>
2019-09-06 11:43:47 -0400

    header added - may need tweaking still

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,30 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    
+    // Create Elements
+    let header = document.createElement("div");
+    let date = document.createElement("span");
+    let title = document.createElement("h1");
+    let temp = document.createElement("span");
+
+    // Add Classes
+    header.classList.add("header");
+    date.classList.add("date");
+    temp.classList.add("temp");
+
+    // Assign children to Parent Elements & add it to .header-container
+    let headerContainer = document.querySelector(".header-container");
+    headerContainer.appendChild(header);
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+    
+    // Static Data given to us
+    date.textContent = `SMARCH 28, 2019`; // This typo is killing me inside!
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°";
+  }
+  
+  Header(); 
